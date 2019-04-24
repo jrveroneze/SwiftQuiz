d18e802d9466eb0b189bb55a6b8ee945433c47ef
Junior Veroneze <oscarveronezejr@hotmail.com>
2019-04-24 17:09:24 -0300

    Aplicação da lógica do quiz.

--- SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/ResultViewController.swift
@@ -14,13 +14,21 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var labelWrong: UILabel!
     @IBOutlet weak var labelScore: UILabel!
     
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        labelAnswered.text = "Perguntas respondidas: \(totalAnswers)"
+        labelCorrect.text = "Perguntas corretas: \(totalCorrectAnswers)"
+        labelWrong.text = "Perguntas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers*100/totalAnswers
+        labelScore.text = "\(score)%"
     }
     
     @IBAction func close(_ sender: UIButton) {
-        
+        dismiss(animated: true, completion: nil)
     }
 }
